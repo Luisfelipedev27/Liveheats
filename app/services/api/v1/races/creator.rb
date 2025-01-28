a0251778ad4e294335c59ba3f4d30927e4fb9ee3
Luisfelipedev27 <luisfelipe1582@hotmail.com>
2025-01-28 00:23:45 -0300

    service CREATOR to create a Race

---
+++ app/services/api/v1/races/creator.rb
@@ -0,0 +1,29 @@
+module Api
+  module V1
+    module Races
+      class Creator < ApplicationService
+        def initialize(race_params:)
+          self.race_params = race_params
+        end
+
+        def call
+          create_race
+
+          self
+        end
+
+        private
+
+        attr_accessor :race_params
+
+        def create_race
+          race = Race.new(race_params)
+
+          return true if race.save
+
+          self.error_message = 'error on create race'
+        end
+      end
+    end
+  end
+end
