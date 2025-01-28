6c530768f7f4327113bf064ed8ba8d01911516aa
Luisfelipedev27 <luisfelipe1582@hotmail.com>
2025-01-28 00:54:32 -0300

    create service to handle with races informations

---
+++ app/services/api/v1/races/finder.rb
@@ -0,0 +1,50 @@
+module Api
+  module V1
+    module Races
+      class Finder < ApplicationService
+        attr_reader :race
+
+        def initialize(race_id:)
+          self.race_id = race_id
+        end
+
+        def call
+          find_race
+
+          self
+        end
+
+        private
+
+        attr_accessor :race_id
+
+        attr_writer :race
+
+        def find_race
+          race = Race.includes(:lanes, :students).find(race_id)
+
+          self.race = race_json(race) if race
+
+          true
+        rescue ActiveRecord::RecordNotFound
+          self.error_message = 'error on find race'
+
+          false
+        end
+
+        def race_json(race)
+          {
+            id: race.id,
+            name: race.name,
+            results: race.lanes.order(:position).map do |lane|
+              {
+                student: lane.student.name,
+                position: lane.position
+              }
+            end
+          }
+        end
+      end
+    end
+  end
+end
---
+++ spec/services/api/v1/races/finder_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::Races::Finder do
+  describe '#call' do
+    context 'when finding a race' do
+      it 'finds a race' do
+        race = create(:race, students: [create(:student), create(:student)])
+
+        service = described_class.call(race_id: race.id)
+
+        expect(service).to be_success
+        expect(service.race).to eq(
+          {
+            id: race.id,
+            name: race.name,
+            results: [
+              { student: race.students.first.name, position: nil },
+              { student: race.students.second.name, position: nil }
+            ]
+          }
+        )
+      end
+    end
+
+    context 'when finding a race that does not exist' do
+      it 'returns an error message' do
+        service = described_class.call(race_id: 1)
+
+        expect(service).not_to be_success
+        expect(service.error_message).to eq('error on find race')
+      end
+    end
+  end
+end
